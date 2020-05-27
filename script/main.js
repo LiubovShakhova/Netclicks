ea3c0d6a58ad2a488ea9453e79aafd42918e2754
LiubovShakhova <63476452+LiubovShakhova@users.noreply.github.com>
2020-05-27 22:01:48 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -41,13 +41,13 @@ const renderCard = response => {
         
         const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
         const backdropIMG = backdrop ? IMG_URL + backdrop : '';
-        const voteValue = vote === 0 ? '' : ${voteValue};
+        const voteValue = vote === 0 ? '' : `<span class="tv-card__vote">${voteValue}</span>`;
 
         const card = document.createElement('li');
         card.className = 'tv-shows__item';
         card.innerHTML = `
         <a href="#" class="tv-card">
-          <span class="tv-card__vote">${voteValue}</span>
+          ${voteValue}
           <img class="tv-card__img"
               src="${posterIMG}"
               data-backdrop="${IMG_URL + backdrop}"
