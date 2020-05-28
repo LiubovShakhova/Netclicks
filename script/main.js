8eaf4118298ec2eb6bf1d06228495bd3081d5db7
LiubovShakhova <63476452+LiubovShakhova@users.noreply.github.com>
2020-05-28 22:32:03 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,11 +1,25 @@
 //Menu
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const SERVER = 'https://api.themoviedb.org/3';
 const API_KEY = '4bc8f24c60123ebaf470e476b9cf276c';
 
 const leftMenu = document.querySelector('.left-menu'),
     hamburger = document.querySelector('.hamburger'),
     tvShowsList = document.querySelector('.tv-shows__list'),
-    modal = document.querySelector('.modal');
+    modal = document.querySelector('.modal'),
+    tvShows = document.querySelector('.tv-shows'),
+    tvCardImg = document.querySelector('.tv-card__img'),
+    modalTitle = document.querySelector('.modal__title'),
+    genresList = document.querySelector('.genres-list'),
+    rating = document.querySelector('.rating'),
+    description = document.querySelector('.description'),
+    modalLink = document.querySelector('.modal__link'),
+    searchForm = document.querySelector('.search__form'),
+    searchFormInput = document.querySelector('.search__form-input');
+
+
+const loading = document.createElement('div');
+loading.className = 'loading';
 
 const DBService = class {
   /*constructor(name. age) {
@@ -17,14 +31,29 @@ const DBService = class {
       if (res.ok) {
         return res.json();
       } else {
-          throw new Error(`Files can not be found here ${url}`)
+          throw new Error(`Files can not be found here ${url}`);
       }
   }
 
   getTestData = () => {
-      return this.getData('test.json')
+      return this.getData('test.json');
   }
+
+  getTestCard = () => {
+    return this.getData('card.json');
+  }
+
+  getSearchResult = query => {
+    return this.getData(`${SERVER}/search/tv?api_key=${API_KEY}&query=${query}&language=ru-RU`)
+  }
+
+  getTvShow = id => {
+    return this.getData(`${SERVER}/tv/${id}?api_key=${API_KEY}&language=en-US`);
+  
 }
+}
+
+console.log(new DBService().getSearchResult('Star Wars'));
 
 const renderCard = response => {
     console.log(response);
@@ -36,31 +65,46 @@ const renderCard = response => {
           backdrop_path: backdrop, 
           name: title, 
           poster_path: poster, 
-          vote_average: vote
-        } = item
+          vote_average: vote,
+          id
+        } = item;
         
         const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
         const backdropIMG = backdrop ? IMG_URL + backdrop : '';
-        const voteValue = vote === 0 ? '' : `<span class="tv-card__vote">${vote}</span>`;
+        const voteValue = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
 
         const card = document.createElement('li');
+
         card.className = 'tv-shows__item';
         card.innerHTML = `
-        <a href="#" class="tv-card">
+        <a href="#" id="${id}" class="tv-card">
           ${voteValue}
           <img class="tv-card__img"
               src="${posterIMG}"
-              data-backdrop="${IMG_URL + backdrop}"
+              data-backdrop="${backdropIMG}"
               alt="${title}">
           <h4 class="tv-card__head">${title}</h4>
         </a>
         `;
+
+        loading.remove();
         tvShowsList.insertAdjacentElement('afterbegin', card);
-        console.log(card);
+        
     });
 };
-new DBService().getTestData().then(renderCard);
 
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const value = searchFormInput.value.trim();
+
+    if(value) {
+      tvShows.append(loading);
+      new DBService().getSearchResult(value).then(renderCard);
+    }
+    searchFormInput.value = '';
+    
+    
+});
 
 
 //Open/ Close Menu
@@ -79,6 +123,7 @@ document.addEventListener('click', event => {
 });
 
 leftMenu.addEventListener('click', event => {
+  event.preventDefault();
   const target = event.target;
   const dropdown = target.closest('.dropdown');
 
@@ -98,8 +143,30 @@ tvShowsList.addEventListener('click', event => {
     const card = target.closest('.tv-card');
     
     if (card) {
-        document.body.style.overflow= 'hidden';
-        modal.classList.remove('hide');
+
+        new DBService().getTvShow(card.id)
+        .then(data => {
+            tvCardImg.src = IMG_URL + data.poster_path;
+            modalTitle.textContent = data.name;
+
+            genresList.textContent = '';
+            //genresList.innerHTML = data.genres.reduce((acc, item) => `${acc}<li>${item.name}</li>`, '');
+            //for (const item of data.genres) {
+                //genresList.innerHTML += `<li>${item.name}</li>`
+            //}
+
+            data.genres.forEach(item => {
+              genresList.innerHTML += `<li>${item.name}</li>`;
+            });
+
+            rating.textContent = data.vote_average;
+            description.textContent = data.overview;
+            modalLink.href = data.homepage;
+        })
+        .then(() => {
+          document.body.style.overflow= 'hidden';
+          modal.classList.remove('hide');
+        })  
     }
 });
 
